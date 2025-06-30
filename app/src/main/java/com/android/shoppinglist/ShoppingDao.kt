25ee9587752be2c8a90db3b57a11663994d736e4
james.barnes <james.barnes@solirius.com>
2025-06-30 16:49:07 +0100

    make ShoppingItem class and Dao

--- app/src/main/java/com/android/shoppinglist/ShoppingDao.kt
+++ app/src/main/java/com/android/shoppinglist/ShoppingDao.kt
@@ -1,4 +1,21 @@
 package com.android.shoppinglist
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
 interface ShoppingDao {
+    //suspend lets us run these as coroutines - async!
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateInsert(item: ShoppingItem)
+
+    @Delete
+    suspend fun delete(item: ShoppingItem)
+
+    @Query("SELECT * FROM shopping_items")
+    fun getAllShoppingItems(): LiveData<List<ShoppingItem>>
 }
--- app/src/main/java/com/android/shoppinglist/ShoppingItem.kt
+++ app/src/main/java/com/android/shoppinglist/ShoppingItem.kt
@@ -1,4 +1,17 @@
 package com.android.shoppinglist
 
-class ShoppingItem {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//data classes are used to hold data
+@Entity(tableName = "shopping_items")
+data class ShoppingItem(
+    @ColumnInfo(name = "item-name")
+    var name: String,
+    @ColumnInfo(name = "item_amount")
+    var amount: Int
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null;
 }
